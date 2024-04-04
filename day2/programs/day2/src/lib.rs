171c7d6f738282da032ad7ccfc0a7b04738ca16c
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2024-04-04 21:51:32 +0500

    day2

---
+++ day2/programs/day2/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HAJKvHJon8JGyrRqdiLf2Zs1czVvyKGwp2WEhrT2sjtn");
+
+#[program]
+pub mod day2 {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
