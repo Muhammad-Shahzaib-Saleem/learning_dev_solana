29e003162796773c46934889ec0cdaabeecdf97d
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2024-04-04 21:46:44 +0500

    Cargo

--- programs/day2/src/lib.rs
+++ programs/day2/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("HAJKvHJon8JGyrRqdiLf2Zs1czVvyKGwp2WEhrT2sjtn");
 pub mod day2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
