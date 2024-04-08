58da9af4fd14b900b5c02d47f158daa5447a7670
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2024-04-09 03:54:03 +0500

    day3 completed

--- day_3/tests/day_3.ts
+++ day_3/tests/day_3.ts
@@ -8,9 +8,36 @@ describe("day_3", () => {
 
   const program = anchor.workspace.Day3 as Program<Day3>;
 
-  it("Is initialized!", async () => {
+  it("Is boaty mc boatface!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.boatyMcBoatface().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Should add", async () => {
+    const tx = await program.methods.add(new anchor.BN(1), new anchor.BN(2)).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Should sub", async () => {
+    const tx = await program.methods.sub(
+      new anchor.BN(10),
+      new anchor.BN(3)).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Should mul", async () => {
+    const tx = await program.methods.mul(
+      new anchor.BN(10),
+      new anchor.BN(3)).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Should div", async () => {
+    const tx = await program.methods.div(
+      new anchor.BN(10),
+      new anchor.BN(3)).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
 });
