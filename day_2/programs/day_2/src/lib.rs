65accadc80427c41354adbedb1b5d3cb2020198a
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2024-04-09 02:45:49 +0500

    day2 is completed

--- day_2/programs/day_2/src/lib.rs
+++ day_2/programs/day_2/src/lib.rs
@@ -7,12 +7,23 @@ pub mod day_2 {
     use super::*;
 
     pub fn initialize(_ctx: Context<Initialize>, a: u64, b: u64, message: String) -> Result<()> {
+        let sub_result = a - b;
+        let add_result = a + b;
+        let mul_result = a * b;
+        let div_result = a / b;
+
         msg!("You said {:?}", message);
         msg!("You sent {} {}", a, b);
+
+        msg!("Value of calculations {}", sub_result);
+        msg!("Value of calculations {}", add_result);
+        msg!("Value of calculations {}", mul_result);
+        msg!("Value of calculations {}", div_result);
+
         Ok(())
     }
 
-    pub fn array(ctx: Context<Initialize>, arr: Vec<u64>) -> Result<()> {
+    pub fn array(_ctx: Context<Initialize>, arr: Vec<u64>) -> Result<()> {
         msg!("Your array {:?}", arr);
         Ok(())
     }
